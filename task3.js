19a8f75bfb26aac5fd3fc741ccad517bc07b2910
Ilwad Kamal <112329600+ilwadk@users.noreply.github.com>
2022-11-08 19:27:33 +0000

    task 2 and 3 done

--- task3.js
+++ task3.js
@@ -1,1 +1,17 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+let multipleOf3 = someNumbers.some(function(numbers){
+    return numbers % 3 === 0
+})
+
+let multipleOf5 = someNumbers.some(function(numbers){
+    return numbers % 5 === 0
+})
+
+let multipleOf60 = someNumbers.some(function(numbers){
+    return numbers % 60 === 0
+})
+
+let multipleOf90 = someNumbers.some(function(numbers){
+    return numbers % 90 === 0
+})
